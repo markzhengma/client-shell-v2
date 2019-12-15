97227228ed1f282bf4352428aa1e27633898d89f
Mark Zheng Ma <mazheng1022@hotmail.com>
2019-12-15 20:33:50 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/client-shell-v2/precache-manifest.13564fae2555d05fe9461b3759bdad84.js"
+  "/client-shell-v2/precache-manifest.5ff2eb55b96ae60e95acce364399f7f1.js"
 );
 
 self.addEventListener('message', (event) => {
