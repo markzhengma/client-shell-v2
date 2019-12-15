2bd6a054430943f2b3b39d0910ef95ddad38a0d1
Mark Zheng Ma <mazheng1022@hotmail.com>
2019-12-15 15:36:01 +0800

    Updates

---
+++ precache-manifest.2aab4651f8bc0aed3f5bd55e761045ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689b377f2cd4310e9c71c3df7c8b895a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3ee29677484da47df7",
+    "url": "/static/css/main.8bb466ef.chunk.css"
+  },
+  {
+    "revision": "80b68f33b4c4b8f01488",
+    "url": "/static/js/2.1ca2f378.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.1ca2f378.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e3ee29677484da47df7",
+    "url": "/static/js/main.8ba81abd.chunk.js"
+  },
+  {
+    "revision": "5a8c95a1c2be586ee825",
+    "url": "/static/js/runtime-main.100dbd27.js"
+  }
+]);
