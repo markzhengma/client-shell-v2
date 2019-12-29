77674e8f50d239795b7117fb7b5fcd19dc72988f
Mark Zheng Ma <mazheng1022@hotmail.com>
2019-12-29 16:32:04 +0800

    new deployment

---
+++ build/precache-manifest.061d3d458d5cef00cdf784cf3aeb3306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d49bb3a529f18399363e60a844901f",
+    "url": "/client-shell-v2/index.html"
+  },
+  {
+    "revision": "07b0571a03fe3f8de309",
+    "url": "/client-shell-v2/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c279b4054e82a50eb462",
+    "url": "/client-shell-v2/static/css/main.8e188b60.chunk.css"
+  },
+  {
+    "revision": "07b0571a03fe3f8de309",
+    "url": "/client-shell-v2/static/js/2.748fb613.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/client-shell-v2/static/js/2.748fb613.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c279b4054e82a50eb462",
+    "url": "/client-shell-v2/static/js/main.aa4ec2eb.chunk.js"
+  },
+  {
+    "revision": "71f8c65c88a609e2086c",
+    "url": "/client-shell-v2/static/js/runtime-main.c6be63d8.js"
+  }
+]);
