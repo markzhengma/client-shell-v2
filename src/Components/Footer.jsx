f94f6910769fcecae9f1a076bf4c57d25ef64d02
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-01-14 13:58:55 +0800

    update footer styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,20 +12,26 @@ class Footer extends Component {
 
   render() {
     return (
-      <Navbar sticky = "bottom" className = "footer">
+      <Navbar sticky = "bottom" className = "footer" style = {{ display: 'flex', flexDirection: 'column', padding: '40px' }}>
         <div className = "footer-info">
-          <h5>呼伦贝尔市佳润物资有限公司</h5>
-          <p>0470-8223779</p>
+          <h5>呼伦贝尔市佳润物资有限公司 | 0470-8223779</h5>
         </div>
-        <div className = "footer-legal">
-          <div style={{ width:'300px', margin:'0 auto', padding:'20px 0'}}>
-            <a target="_blank" href="http://beian.miit.gov.cn" style={{display:'inline-block',textDecoration: 'none', height:'20px', lineHeight:'20px'}}><img src="" style={{float:'left'}}/><p style={{float:'left',height:'20px',lineHeight:'20px',margin: '0px 0px 0px 5px', color:'#939393'}}>蒙ICP备19005903号-1</p></a>
-          </div>
-          {/* <a href = "http://beian.miit.gov.cn" target="_blank">蒙ICP备19005903号-1</a> */}
-          <div style={{ width:'300px', margin:'0 auto', padding:'20px 0'}}>
-            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=15070202000139" style={{display:'inline-block',textDecoration: 'none', height:'20px', lineHeight:'20px'}}><img src="" style={{float:'left'}}/><p style={{float:'left',height:'20px',lineHeight:'20px',margin: '0px 0px 0px 5px', color:'#939393'}}>蒙公网安备 15070202000139号</p></a>
-          </div>
-		 
+        <div className = "footer-info">
+            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=15070202000139" style={{display:'inline-block',textDecoration: 'none', height:'20px', lineHeight:'20px'}}>
+              <img src={require('../Images/beian_2.png')} style={{float:'left'}}/>
+              <p style={{float:'left',height:'20px',lineHeight:'20px',margin: '0px 0px 0px 5px', color:'#939393'}}>
+                蒙公网安备 15070202000139号
+              </p>
+            </a>
+            <p style={{float:'left',height:'20px',lineHeight:'20px',margin: '0px 0px 0px 5px', color:'#939393'}}>
+                |
+            </p>
+            <a target="_blank" href="http://beian.miit.gov.cn" style={{display:'inline-block',textDecoration: 'none', height:'20px', lineHeight:'20px'}}>
+              <img src="" style={{float:'left'}}/>
+              <p style={{float:'left',height:'20px',lineHeight:'20px',margin: '0px 0px 0px 5px', color:'#939393'}}>
+                蒙ICP备19005903号-1
+              </p>
+            </a>
         </div>
       </Navbar>
     )
