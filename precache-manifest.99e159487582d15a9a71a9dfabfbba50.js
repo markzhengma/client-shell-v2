087dbd9eceb0d915a74d5487fd9bb8754f3ca63d
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-01-20 11:27:51 +0800

    Updates

--- precache-manifest.99e159487582d15a9a71a9dfabfbba50.js
+++ precache-manifest.99e159487582d15a9a71a9dfabfbba50.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa89fb490e0f77270f4269300db7d9c1",
+    "revision": "7be4a8b03dd4850c6dd78881655320ff",
     "url": "/client-shell-v2/index.html"
   },
   {
-    "revision": "d92798f266be21c48f03",
+    "revision": "93e70b3bf5c2bc143016",
     "url": "/client-shell-v2/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "7fdfb9e0905580ec265a",
+    "revision": "51523931efef616781b1",
     "url": "/client-shell-v2/static/css/main.41c74a72.chunk.css"
   },
   {
-    "revision": "d92798f266be21c48f03",
-    "url": "/client-shell-v2/static/js/2.09575d84.chunk.js"
+    "revision": "93e70b3bf5c2bc143016",
+    "url": "/client-shell-v2/static/js/2.5c1b073d.chunk.js"
   },
   {
-    "revision": "744b76f766044157687ca8b61b891873",
-    "url": "/client-shell-v2/static/js/2.09575d84.chunk.js.LICENSE"
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/client-shell-v2/static/js/2.5c1b073d.chunk.js.LICENSE"
   },
   {
-    "revision": "7fdfb9e0905580ec265a",
-    "url": "/client-shell-v2/static/js/main.8588df1b.chunk.js"
+    "revision": "51523931efef616781b1",
+    "url": "/client-shell-v2/static/js/main.15467ca3.chunk.js"
   },
   {
     "revision": "71f8c65c88a609e2086c",
