1cd2fa1dc8f7b7c0b96449cc6a1438c959245938
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-02-15 16:43:58 +0800

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,15 +17,12 @@ class Footer extends Component {
           <h5>呼伦贝尔市佳润物资有限公司 | 0470-8223779</h5>
         </div>
         <div className = "footer-info">
-            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=15070202000139" style={{display:'inline-block',textDecoration: 'none', height:'20px', lineHeight:'20px'}}>
+            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=15070202000140" style={{display:'inline-block',textDecoration: 'none', height:'20px', lineHeight:'20px'}}>
               <img src={require('../Images/beian_2.png')} style={{float:'left'}}/>
               <p style={{float:'left',height:'20px',lineHeight:'20px',margin: '0px 0px 0px 5px', color:'#939393'}}>
-                蒙公网安备 15070202000139号
+                蒙公网安备 15070202000140号
               </p>
             </a>
-            <p style={{float:'left',height:'20px',lineHeight:'20px',margin: '0px 0px 0px 5px', color:'#939393'}}>
-                |
-            </p>
             <a target="_blank" href="http://beian.miit.gov.cn" style={{display:'inline-block',textDecoration: 'none', height:'20px', lineHeight:'20px'}}>
               <img src="" style={{float:'left'}}/>
               <p style={{float:'left',height:'20px',lineHeight:'20px',margin: '0px 0px 0px 5px', color:'#939393'}}>
